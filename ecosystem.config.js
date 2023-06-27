1c345792390af18f3c018eabd878745870808b44
Federico Lopez <federico.lopez@skytel.com.py>
2023-06-27 10:33:20 -0400

    cambiamos el puerto

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'todo-app', // Nombre de tu aplicación
       script: 'ng', // Comando para ejecutar Angular CLI
-      args: 'serve', // Argumentos adicionales para el comando
+      args: 'serve --port 8001', // Argumentos adicionales para el comando
       instances: 1, // Número de instancias que deseas ejecutar
       autorestart: true, // Reiniciar automáticamente la aplicación en caso de error
       watch: false, // No observar cambios en los archivos
