848c6b4deaaf9548651135e87be6e92e9c5d5fed
Federico Lopez <federico.lopez@skytel.com.py>
2023-06-27 10:29:54 -0400

    se agrega el ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'todo-app', // Nombre de tu aplicación
+      script: 'ng', // Comando para ejecutar Angular CLI
+      args: 'serve', // Argumentos adicionales para el comando
+      instances: 1, // Número de instancias que deseas ejecutar
+      autorestart: true, // Reiniciar automáticamente la aplicación en caso de error
+      watch: false, // No observar cambios en los archivos
+      max_memory_restart: '1G', // Reiniciar la aplicación si supera este límite de memoria
+      env: {
+        NODE_ENV: 'production' // Variable de entorno (puedes personalizarla según tus necesidades)
+      }
+    }
+  ]
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://69.164.221.100:9001',
+};
